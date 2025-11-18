c271ed90b3ceb462d9ec83c8ed26aa0992559866
MarlaisW <168648385+MarlaisW@users.noreply.github.com>
2025-11-18 09:42:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ pd_df = my_dataframe.to_pandas()
 #st.stop()
 
 ingredients_list = st.multiselect( 
-    'Choose up to 5 ingeredients:', 
+    'Choose up to 5 ingredients:', 
     my_dataframe,
     max_selections=5
 )
